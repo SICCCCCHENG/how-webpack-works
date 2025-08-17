632ce5bebb2bd542e1526ccd6073862894e94ed5
sicheng <zsc0373@outlook.com>
2025-08-17 22:49:18 +0800

    获取依赖的模块id 可运行 p7

--- webpack/NormalModule.js
+++ webpack/NormalModule.js
@@ -1,5 +1,10 @@
+const path = require('path');
+const types = require('babel-types');
+const generate = require('babel-generator').default;
+const traverse = require('babel-traverse').default;
+
 class NormalModule {
-    constructor({ name, context, rawRequest, resource, parser }) {
+    constructor({ name, context, rawRequest, resource, parser, moduleId }) {
         this.name = name;
         // /Users/sicheng/Desktop/Demo/learn/webpack-learn
         this.context = context;
@@ -7,15 +12,52 @@ class NormalModule {
         this.rawRequest = rawRequest;
         // /Users/sicheng/Desktop/Demo/learn/webpack-learn/src/index.js
         this.resource = resource;  // 模块的绝对路径
+        this.moduleId = moduleId;
         this.parser = parser;  // ast 解析器,把源代码解析成ast
         this._source = null;  // 模块对应的源代码
         this._ast = null;  // 对应的ast
+        this.dependencies = [];
     }
     //解析依赖
     build(compilation, callback) {
         this.doBuild(compilation, err => {
-            this._ast = this.parser.parse(this._source);
-            callback();
+            let originalSource = this.getSource(this.resource, compilation);
+            // 将 当前模块 的内容转换成 AST
+            const ast = this.parser.parse(originalSource);
+            // 遍历语法树,找到里面的依赖进行收集
+            traverse(ast, {
+                // 如果当前节点是一个函数调用时, 进入回调
+                CallExpression: (nodePath) => {
+                    let node = nodePath.node;
+                    // 当前节点是 require 时
+                    if (node.callee.name === 'require') {
+                        //修改require为__webpack_require__
+                        node.callee.name = '__webpack_require__';
+                        //获取要加载的模块ID
+                        let moduleName = node.arguments[0].value;
+                        //获取扩展名
+                        let extension = moduleName.split(path.posix.sep).pop().indexOf('.') == -1 ? '.js' : '';
+                        //获取依赖模块的绝对路径   posix 是将 window 里面 \ 转化为 / 
+                        // /Users/sicheng/Desktop/Demo/learn/webpack-learn/src/title.js
+                        let dependencyResource = path.posix.join(path.posix.dirname(this.resource), moduleName + extension);
+                        //获取依赖模块的模块ID 
+                        // ./+从根目录出发到依赖模块的绝对路径的相对路径
+                        let dependencyModuleId = '.' + path.posix.sep + path.posix.relative(this.context, dependencyResource);
+
+                        console.log(dependencyModuleId, 'dependencyModuleId');
+                        // //添加依赖
+                        // this.dependencies.push({
+                        //     name: this.name, context: this.context, rawRequest: moduleName,
+                        //     moduleId: dependencyModuleId, resource: dependencyResource
+                        // });
+                        // node.arguments = [types.stringLiteral(dependencyModuleId)];
+                    }
+                }
+            });
+            // let { code } = generate(ast);
+            // this._source = code;
+            // this._ast = ast;
+            // callback();
         });
     }
     //获取模块代码
@@ -38,4 +80,14 @@ module.exports = NormalModule;
  * 4. 分析ast里面的依赖,也就是找require import节点,分析依赖的模块
  * 5. 递归编译依赖的模块
  * 6. 不停依次递归执行以上5步, 直到所有的模块都编译完成为止
+ */
+
+/**
+ * 模块id的问题:
+ * 不管是相对的本地模块,还是三方模块
+ * 最后 moduleId 都是相对于项目根目录相对路径
+ * ./src/title.js
+ * ./src/index.js
+ * ./node_modules/util/util.js
+ * 路径分隔符是linux下的 / , 而不是window的 \
  */
